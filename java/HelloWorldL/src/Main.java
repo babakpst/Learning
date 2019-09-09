69f499a4cb7bde9543e82f95544065bd95108100
babakpst <babakpst@gmail.com>
2019-09-08 20:24:25 -0500

    input variables

--- java/HelloWorldL/src/Main.java
+++ java/HelloWorldL/src/Main.java
@@ -1,8 +1,12 @@
 import java.awt.*;
 import java.util.concurrent.atomic.AtomicReferenceFieldUpdater;
+import java.util.Scanner;
 
 public class Main {
 public static void main(String[] args){
+
+    Scanner sc = new Scanner(System.in);
+
     System.out.println("Hello world!");
     System.out.println("Pilot Babak is speaking!");
 
@@ -15,6 +19,31 @@ public static void main(String[] args){
     myCar.chagnePaintColor(Color.RED);
 
     System.out.println(" My new car color is: " + myCar.paintColor.toString());
+
+
+    //String userInput = "entertainment";  // Strings are immutable
+    System.out.println(" Press your fingers on the keyboard please: ");
+    String userInput = sc.next();  // Strings are immutable
+    String upperCased = userInput.toUpperCase();
+    System.out.println(" This is the lower cased of the variable: " + userInput +  " and the upper cased: " + upperCased );
+
+    char firstChar = userInput.charAt(0);
+    char secondChar = userInput.charAt(1);
+
+    System.out.println(" first char: " + firstChar + " and the second char: "  + secondChar);
+
+    System.out.println(" contains enter? " + userInput.contains("enter"));
+    System.out.println(" contains Enter? " + userInput.contains("Enter"));
+    System.out.println(" contains Enter? " + userInput.contains("Enter".toLowerCase()));
+
+    System.out.println(" Print an interger: ");
+    int userInt = sc.nextInt();
+    System.out.println(" Interger: " + userInt);
+
+    System.out.println(" Print a double: ");
+    double userDbl = sc.nextDouble();
+    System.out.println(" double: " + userDbl);
+
 
 }
 }
