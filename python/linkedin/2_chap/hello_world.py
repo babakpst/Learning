4cd8c88abd6158bc38fc817f418d08ea436b95b0
babakpst <babakpst@gmail.com>
2019-09-15 15:49:31 -0500

    starting working on the linkedin class for python

---
+++ python/linkedin/2_chap/hello_world.py
@@ -0,0 +1,15 @@
+
+# Babak Poursartip
+# September 15, 2019
+# Starting on the online course in linkedin: 
+# https://www.linkedin.com/learning/learning-python-2/building-hello-world?u=2163426
+
+def main():
+  print("An empty line.")
+  print("Hello world.")
+
+
+# calling the main function
+if __name__ == "__main__":
+  #print("I am here dude.")
+  main()
