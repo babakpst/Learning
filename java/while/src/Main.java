9640219aad8964baa33b237e7cc1bba019de80a2
babakpst <babakpst@gmail.com>
2019-09-14 22:55:47 -0500

    while

---
+++ java/while/src/Main.java
@@ -0,0 +1,18 @@
+public class Main {
+
+public static void main(String[] args){
+
+
+    System.out.println(" This is the while loop: ");
+    int x = 3;
+    while (x>0){
+        System.out.println(" current x: " + x);
+        x = x-1;
+    }
+
+    System.out.println("Final x: " + x);
+    
+}
+
+
+}
