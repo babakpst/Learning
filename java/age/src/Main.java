d0295eb67cd99c8c3105e9c22cece429be88fd2f
babakpst <babakpst@gmail.com>
2019-09-14 22:42:59 -0500

    if condition

--- java/age/src/Main.java
+++ java/age/src/Main.java
@@ -1,2 +1,27 @@
+import java.util.Scanner;
+
 public class Main {
+
+    public static void main(String[] args){
+        System.out.println(" Print age here: ");
+        Scanner in = new Scanner(System.in);
+
+        int age = in.nextInt();
+
+        if (age >= 0 && age <= 5){
+            System.out.println("baby");
+        }else if (age >= 6 && age <= 11){
+            System.out.println("kid");
+        }else if (age >= 12 && age <= 17){
+            System.out.println("teen");
+        }else if (age > 17) {
+            System.out.println("adult");
+        }else{
+            System.out.println("wrong age");
+        }
+    System.out.println("This is the end!");
+
+    }
+
+
 }
