913a61e6c485781832cc1dc9a5c460805c8a8ab1
babakpst <babakpst@gmail.com>
2019-09-14 23:00:52 -0500

    do while+for

---
+++ java/doWhile/src/Main.java
@@ -0,0 +1,24 @@
+public class Main {
+
+
+
+        public static void main(String[] args){
+
+
+            System.out.println(" This is the while loop: ");
+            int x = 3;
+            do {
+                System.out.println(" current x: " + x);
+                x = x-1;
+            } while (x>0);
+            System.out.println("Final x: " + x);
+
+
+            for (int i = 0; i < 10; i++){
+
+                System.out.println(" This is i: " + i);
+            }
+
+        }
+
+}
--- java/while/src/Main.java
+++ java/while/src/Main.java
@@ -11,7 +11,7 @@ public static void main(String[] args){
     }
 
     System.out.println("Final x: " + x);
-    
+
 }
 
 
