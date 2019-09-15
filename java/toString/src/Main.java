fafb625df5bbcd62ac490453f6afd4f8c0620f31
babakpst <babakpst@gmail.com>
2019-09-14 21:49:18 -0500

    replace

---
+++ java/toString/src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+
+    public static void main(String[] args){
+        String s = "dog";
+        System.out.println(" This is the old word: " + s);
+        String replacedF = s.replace('d', 'f');
+        System.out.println(" This is the new word: " + replacedF);
+
+    }
+
+
+}
