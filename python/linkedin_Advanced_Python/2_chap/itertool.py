02887aedb11afab2f48cf2e6c2463a4cba3d371b
babakpst <babakpst@gmail.com>
2019-10-25 23:39:00 -0500

    itertools

--- python/linkedin_Advanced_Python/2_chap/itertool.py
+++ python/linkedin_Advanced_Python/2_chap/itertool.py
@@ -6,6 +6,10 @@
 
 import itertools
 
+
+def testfunction(x):
+  return x < 40
+
 def main():
   # cycle iterator can be used to cycle over a collection
   seq1  = ["Babak", "Shiva", "Behnaz", "Behrang"]
@@ -21,7 +25,33 @@ def main():
   print (next(cycle1))
   print (next(cycle1))
 
+  # Create a counter
+  count1= itertools.count(100,10) # 100 is the start value, 10 is the increment
+  print(next(count1))
+  print(next(count1))
+  print(next(count1))
+  print(next(count1))
+  print(next(count1))
 
+  # Accumulate creates an iterator that accumulates values
+  values = [10, 20, 30, 40, 50, 40, 30, 20, 10]
+  acc = itertools.accumulate(values)
+  # print(acc) # this does not do anything.
+  print(list(acc))
+
+  acc2 = itertools.accumulate(values, max)
+  print(list(acc2))
+
+  # use CHAIN to connect sequences together
+  xx = itertools.chain("ABCO", "1234")
+  print(list(xx))
+
+  # The dropwhile and takewhile will return values until a certain condition is met that stops themdatetime A combination of a date and a time. 
+  bb = itertools.dropwhile(testfunction, values) # drops values while the testfunction returns true
+  print(list(bb))
+
+  bb = itertools.takewhile(testfunction, values)  # return values while the testfunction returns true
+  print(list(bb))
 
 if __name__=="__main__":
   main()
