52c27cdb6ab4bf0d517229ee1b47d0cd64488009
babakpst <babakpst@gmail.com>
2019-10-28 23:17:48 -0500

    lambda

---
+++ python/linkedin_Advanced_Python/3_chap/lambda_func.py
@@ -0,0 +1,24 @@
+
+# Babak Poursartip  
+# 28 Oct 2019
+
+# Lambda Function
+
+# Conversts Celsius to Fahrenheit
+def CelsiusToFahrenheit(temp):
+    return (temp * 9/5) + 32
+
+# Converts Fahrenheit to Celsisus
+def FahrenheitToCelsius(temp):
+    return (temp-32) * 5/9
+
+def main():
+  """ main()=> lambda function
+  """
+  
+
+  
+
+
+if __name__= "__main__":
+  main()
