6c7deb117a5349fd877ca1d1f13f65399f4c3ad3
babakpst <babakpst@gmail.com>
2019-10-29 23:33:33 -0500

    lambda function

--- python/linkedin_Advanced_Python/3_chap/lambda_func.py
+++ python/linkedin_Advanced_Python/3_chap/lambda_func.py
@@ -15,10 +15,17 @@ def FahrenheitToCelsius(temp):
 def main():
   """ main()=> lambda function
   """
-  
+  C_temps = [10, 15, 18, 25]  
+  F_temps = [30, 32, 50, 60, 100]
 
-  
+  # convert C to F
+  print(list(map(FahrenheitToCelsius, F_temps))) # this is great
+  print(list(map(CelsiusToFahrenheit, C_temps)))
+
+  # lambda
+  print(list(map(lambda t:(t-32) * 5/9, F_temps))) # this is great
+  print(list(map(lambda t:(t * 9/5) + 32, C_temps)))
 
 
-if __name__= "__main__":
+if __name__== "__main__":
   main()
