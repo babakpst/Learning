48b09d0b338be8d57162b62e691ab4135db71211
babakpst <babakpst@gmail.com>
2020-03-07 16:34:01 -0600

    pillow

--- python/pillow/pillow.py
+++ python/pillow/pillow.py
@@ -11,54 +11,65 @@ def main():
 
   print("{}".format(" pillow image processing code starts ..."))
 
+
+  # Reading images from files ==================================================
   print("{}".format(" reading images ..."))
-  Image1 = Image.open("fig1_2x1BasicProblem01/1.png")
-  Image2 = Image.open("fig1_2x1BasicProblem01/6.png")
-  Image1.show()
-  Image2.show()  
-  
+  Image1 = Image.open("fig2_B-10/A.png").convert("L")
+  Image2 = Image.open("fig2_B-10/B.png").convert("L")
+ 
+  # If you have an L mode image, that means it is a single channel image - normally interpreted as greyscale. The L means that is just stores the Luminance. It is very compact, but only stores a greyscale, not colour.
+
   print("{}".format(" convert the image to an array ... "))
 
-  Image1_ar_int = np.array(Image1, dtype=np.int16)
-  Image2_ar_int = np.array(Image2, dtype=np.int16)
-  
+  #Image1_ar_int = np.array(Image1)
+  Image1_ar_int = np.array(Image1, dtype=np.uint8)
+  Image2_ar_int = np.array(Image2, dtype=np.uint8)
+
+  Image1_ar_int2 = np.array(Image1, dtype=np.int16)
+  Image2_ar_int2 = np.array(Image2, dtype=np.int16)
+
+  #Image1_ar_int[Image1_ar_int<0] = 0
+  #Image1_ar_int[Image1_ar_int>255] = 255
+
+
   print("{} {}".format("size of matrix:", Image1_ar_int.shape))
   print("{} {}".format("size of matrix:", Image2_ar_int.shape))
 
+  print("{} {}".format("size of matrix:", Image1_ar_int2.shape))
+  print("{} {}".format("size of matrix:", Image2_ar_int2.shape))
 
-  w, h = 512, 512
-  data = np.zeros((h, w, 3), dtype=np.uint8)
-  data[0:256, 0:256] = [255, 0, 0] # red patch in upper left
-  img = Image.fromarray(data, 'RGB')
-  img.show()
-
-
-  #Image.fromarray(Image1_ar_int).convert("L").show()
-  #wait = input("PRESS ENter")
-
-  #Im_n1 = Image1_ar_int  - Image2_ar_int
-  #Im_n2 = Image2_ar_int  - Image1_ar_int
-
-  #Image.fromarray(Im_n1).convert("L").show()
-  #wait = input("PRESS ENter")  
-  #Image.fromarray(Im_n2).convert("L").show()
-  #wait = input("PRESS ENter")
+  print(" arrays:")
+  print(Image1_ar_int)
 
 
-  #res_1 = Image.fromarray(Im_n1)
-  #res_2 = Image.fromarray(Im_n2)
-  #res_1.show()
-  #res_2.show()
-
-  Image1_ar = np.array(Image1)
-  Image2_ar = np.array(Image2)
-  print("{} {}".format("size of matrix:", Image1_ar.shape))
-  print("{} {}".format("size of matrix:", Image2_ar.shape))
+  print(" arrays:")
+  print(Image1_ar_int2)
 
 
-  # convert back to image:  Image.fromarray(im2arr)
+  # Transformations ============================================================
 
 
+
+  # Addition/Subtraction =======================================================
+  Image.fromarray(Image1_ar_int).convert("L").show()
+  #Image.fromarray(Image1_ar_int2).show()
+  wait = input("image1")
+  Image.fromarray(Image2_ar_int).convert("L").show()
+  #Image.fromarray(Image2_ar_int2).show()
+  wait = input("image2")
+
+  Im_n1 = Image1_ar_int  - Image2_ar_int
+  Im_n2 = Image2_ar_int  - Image1_ar_int
+
+  Image.fromarray(Im_n1).convert("L").show()
+  #Image.fromarray(Im_n1).show()
+  wait = input("image1 - image 2")  
+  Image.fromarray(Im_n2).convert("L").show()
+  #Image.fromarray(Im_n2).show()
+  wait = input("image2 - image1")
+
+
+  
   check = np.zeros((5,3))
   print(check.size, check.shape)
 
