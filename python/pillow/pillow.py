e7fa407423a96aa9437b82ea4d7e2c8b39cd4f13
babakpst <babakpst@gmail.com>
2020-03-21 10:21:40 -0500

    pillow: intersection and union

--- python/pillow/pillow.py
+++ python/pillow/pillow.py
@@ -39,38 +39,77 @@ def main():
   print("{} {}".format("size of matrix:", Image2_ar_int2.shape))
 
   print(" arrays:")
-  print(Image1_ar_int)
-
+  #print(Image1_ar_int)
+  print(Image1_ar_int[100,100])
+  print(Image1_ar_int2[100,100])
 
   print(" arrays:")
   print(Image1_ar_int2)
 
 
-  # Transformations ============================================================
+  # Transformations ===========================================================
 
 
 
-  # Addition/Subtraction =======================================================
-  Image.fromarray(Image1_ar_int).convert("L").show()
+  # Addition/Subtraction ======================================================
+  #Image.fromarray(Image1_ar_int).convert("L").show()
   #Image.fromarray(Image1_ar_int2).show()
-  wait = input("image1")
-  Image.fromarray(Image2_ar_int).convert("L").show()
+  #wait = input("image1")
+  #Image.fromarray(Image2_ar_int).convert("L").show()
   #Image.fromarray(Image2_ar_int2).show()
-  wait = input("image2")
+  #wait = input("image2")
 
   Im_n1 = Image1_ar_int  - Image2_ar_int
   Im_n2 = Image2_ar_int  - Image1_ar_int
 
-  Image.fromarray(Im_n1).convert("L").show()
+  #Image.fromarray(Im_n1).convert("L").show()
   #Image.fromarray(Im_n1).show()
-  wait = input("image1 - image 2")  
-  Image.fromarray(Im_n2).convert("L").show()
+  #wait = input("image1 - image 2")  
+  #Image.fromarray(Im_n2).convert("L").show()
   #Image.fromarray(Im_n2).show()
-  wait = input("image2 - image1")
+  #wait = input("image2 - image1")
 
 
+  # intersection and union  ===================================================
+  #Image3 = Image1.open("fig2_B-10/A.png").convert("1")
+  #Image4 = Image2.open("fig2_B-10/B.png").convert("1")
+
+  image3_array = np.asarray(Image1.convert('1'), dtype=np.float32)
+  image4_array = np.asarray(Image2.convert('1'), dtype=np.float32)
+
+  union = image3_array + image4_array
+  #print(union)
+  union = np.where(union > 1., 1., 0)
+  #print(union)
+  #Image.fromarray(intersect).convert("1").show()
+  Image.fromarray(union*255).show()
+  #Image.fromarray(Image1_ar_int2).show()
+  wait = input("union")
+
+  intersect = image3_array + image4_array
+  #print(intersect)
+  intersect = np.where(intersect >= 1., 1., 0)
+  #print(intersect)
+  #Image.fromarray(intersect).convert("1").show()
+  Image.fromarray(intersect*255).show()
+  #Image.fromarray(Image1_ar_int2).show()
+  wait = input("intersect")
+
+  # union =====================================================================
+
+
+
+  # numpy =====================================================================
+  print(" NumPy: ---")
+  Image1_ar1 = np.array(Image1.convert("1"), dtype=np.float32)
+  Image1_ar2 = np.asarray(Image1.convert("L"), dtype=np.float32)
   
+  print(" size:", Image1_ar1.shape, Image1_ar2.shape)
+  print(Image1_ar1)
+  print(Image1_ar2)
+
   check = np.zeros((5,3))
+  print(check[2,2])
   print(check.size, check.shape)
 
   print("{}".format(" End of the code successfully."))
---
+++ python/src2/array.py
@@ -0,0 +1,19 @@
+import numpy as np
+
+def main():
+
+    
+  print(" The code starts ....")
+
+  size  = 5
+  #ar = np.zeros(size,size, dtype=np.float)
+  ar = np.zeros((size,size),  dtype=np.float)
+
+  print(" Here is the array:")
+  print(ar)
+  
+  print(" End of the code. ")
+
+
+if __name__=="__main__":
+  main()
