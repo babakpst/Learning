c49104f5b53324f9a3138a0240e1ba303a463f8d
babakpst <babakpst@gmail.com>
2020-05-02 16:19:14 -0500

    chapter 1: part 4

---
+++ C++/101_linkedin_advanced_topics_in_cpp/01_chap_4_constructors_destructors/constructors.cpp
@@ -0,0 +1,99 @@
+
+/*
+Babak Poursartip
+05/02/2020
+
+Linkedin: advanced topics in cpp
+
+chapter 1: constructors and destructors
+important notes:
+
+implicit op
+1- default constructor
+2- copy constructor
+3- copy operator assignment
+4- destructor
+
+*/
+
+#include <cstdio>
+#include <string>
+
+const std::string unk = "unknown";
+const std::string clone_prefix = "clone-";
+
+// interface
+class Animal {
+  std::string _type = "";
+  std::string _name = "";
+  std::string _sound = "";
+
+public:
+  Animal(); // default constructor
+  Animal(const std::string &type, const std::string &name,
+         const std::string &sound);
+  Animal(const Animal &); // copy constructor
+  Animal &operator=(const Animal &rhs);
+  ~Animal(); // destructor
+
+  void print() const;
+};
+//==============================================================================
+// implementation
+Animal::Animal() : _type(unk), _name(unk), _sound(unk) {
+  puts("default constructor");
+};
+
+Animal::Animal(const std::string &type, const std::string &name,
+               const std::string &sound)
+    : _type(type), _name(name), _sound(sound) {
+  puts("constructor you built with arguments.");
+};
+
+Animal::Animal(const Animal &rhs) {
+  puts("copy constructor");
+  _type = rhs._type;
+  _name = clone_prefix + rhs._name;
+  _sound = rhs._sound;
+};
+
+Animal::~Animal() {
+  printf(" destructor: %s the %s \n", _name.c_str(), _type.c_str());
+};
+
+void Animal::print() const {
+
+  printf(" %s the %s says %s \n", _name.c_str(), _type.c_str(), _sound.c_str());
+}
+
+Animal &Animal::operator=(const Animal &rhs) {
+  puts("copy operator");
+  if (this != &rhs) {
+    _name = clone_prefix + rhs._name;
+    _type = rhs._type;
+    _sound = rhs._sound;
+  }
+  return *this;
+};
+
+//==============================================================================
+int main() {
+
+  printf(" program starts ... \n\n");
+
+  Animal a;
+  a.print();
+
+  const Animal b("cat", "fluffy", " meow");
+  b.print();
+
+  const Animal c = b;
+  c.print();
+
+  a = c;
+  a.print();
+
+  printf(" \n program ends! \n");
+
+  return 0;
+}
