ea5d66e90eb7fce9213afe5008ccbacd27874af5
babakpst <babakpst@gmail.com>
2020-05-03 09:36:56 -0500

    cleaning

--- C++/101_linkedin_advanced_topics/01_chap_1_define_a_class/src/c1_cls/c1_cls.cpp
+++
@@ -1,8 +0,0 @@
-// Linkein:  Advanced topics in c++
-// Babak Poursartip
-// 04/26/2020
-
-#include "../../include/c1_cls/c1_cls.h"
-
-void c1_cls::setvalue(const int &value) { i = value; }
-int c1_cls::getvalue() const { return i; } // this method member is const saved.
