3d4f2d26960c2896895e2b1b0750c2f075d8e32e
babakpst <babakpst@gmail.com>
2020-05-24 11:42:07 -0500

    fixed the warning for exception

--- C++/00_general/22_Exception_Files/main.cpp
+++ C++/00_general/22_Exception_Files/main.cpp
@@ -19,26 +19,12 @@ OutputFile.exceptions (std::ofstream::failbit | std::ofstream::badbit);
 std::cout<< " OK, Lets get started:" << std::endl;
 
 try{
-
 std::cout<<" Here is inside the Try "<< std::endl;
-
 InputFile.open  ("address.txt", std::ios::in );
-
-OutputFile.open ("results.txt", std::ios::out );
-
-OutputFile << " This is the results file: " ;// >> std::endl;
-std::cout << " We just wrote something in the file." << std::endl;
-
-}
-catch(std::ifstream::failure &inputerr ){   // here is the catch for opening the input file.
+} catch(std::ifstream::failure &inputerr ){   // catch for opening the input file.
 std::cout<<" Error while opening a file: "<< std::endl;
 std::cout<<" The error message is: "<< inputerr.what() << std::endl;
 }
-catch(std::ofstream::failure &writeerr){      // here is the catch for 
-std::cout<<" Error while writing in the file: "<< std::endl;
-std::cout<<" The error message is: "<< writeerr.what() << std::endl;
-}
-
 
 // reading from the input file
 int num;
@@ -81,6 +67,18 @@ std::cout << "\n the input: \n";
     index =it - list.begin();
     if (index%2) std::cout << "\n";
   }
+
+
+// create an output file
+try{
+  std::cout<<" Here is inside the Try for output "<< std::endl;
+  OutputFile.open ("results.txt", std::ios::out );
+  OutputFile << " This is the results file: " ;// >> std::endl;
+  std::cout << " We just wrote something in the file." << std::endl;
+}catch(std::ofstream::failure &writeerr){      // catch for the output file
+  std::cout<<" Error while writing in the file: "<< std::endl;
+  std::cout<<" The error message is: "<< writeerr.what() << std::endl;
+}
 
 return 0;
 }
