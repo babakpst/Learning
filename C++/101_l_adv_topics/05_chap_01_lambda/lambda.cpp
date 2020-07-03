bac963c2f8ca8e2cac08e8b3acde2a50d1fc065a
babakpst <babakpst@gmail.com>
2020-07-03 12:19:28 -0500

    lambda function

--- C++/101_l_adv_topics/05_chap_01_lambda/lambda.cpp
+++ C++/101_l_adv_topics/05_chap_01_lambda/lambda.cpp
@@ -1,9 +1,15 @@
 
 // lambda.cpp by Bill Weinman <http://bw.org/>
 
-// lambda: a function w/o name
 
-// enclosure: an encapsulated block of the code
+// REMARKS:
+// - lambda: a function w/o name
+// - closure: an encapsulated block of the code
+// - a lambda funciton creates a closure.
+// - read about stl::transform here: https://www.geeksforgeeks.org/transform-c-stl-perform-operation-elements/
+
+// The goal of this code to use (lambda function or a class), and converts a text to first letter 
+// capitalized.
 
 #include <cstdio>
 #include <algorithm>
@@ -12,24 +18,46 @@
 
 constexpr size_t _maxlen = 128;
 
+// This class is a functor
 class title {
-  char lastc;
-public:
-  title() :lastc(0){}
-  char operator () (const char &c);
+    char lastc;
+  public:
+    title() :lastc(0){}
+    char operator () (const char &c);
 };
 
 
 char title::operator() ( const char &c){
+    printf("%c -",lastc);
+    // 
     const char r = (lastc == ' ' || lastc == 0) ? toupper(c) : tolower(c);
     lastc = c;
+    //puts(lastc);
+    printf("%c %c %c \n",lastc, c, r);
     return r;
 }
 
 int main() {
-    char s[] = "Life is too short not to enjoy it.";
-    std::transform( s, s + strnlen(s, _maxlen), s, title() );
+    char s[] = "life is too short not to enjoy it.";
+
+    // Transform is from the algorithm library.
+
+    // option 1: using the class
+    //std::transform( s, s + strnlen(s, _maxlen), s, title() );
+
+    // option 2: using the lambda function
+    // inside [] in the lambda function is called capture, here it captures a reference to lastc. 
+    // -> in lambda is the function return operator. Here the function returns a char. 
+
+    char lastc=0;
+    std::transform( s, s + strnlen(s, _maxlen), s, [&lastc](const char &c){
+    const char r = (lastc == ' ' || lastc == 0) ? toupper(c) : tolower(c); 
+    lastc = c;
+    return r; } );
+
     puts(s);
+
+
     
     return 0;
 }
