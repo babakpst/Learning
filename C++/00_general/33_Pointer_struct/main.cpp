2b836533bc367292996642b8736d993a988fabb3
babakpst <babakpst@gmail.com>
2020-07-15 09:12:56 -0500

    struct with vector passing to function

--- C++/00_general/33_Pointer_struct/main.cpp
+++ C++/00_general/33_Pointer_struct/main.cpp
@@ -9,16 +9,27 @@ struct pointer
 
 #include <iostream>
 #include <sstream>
+#include <vector>
+#include <iterator>
+
 
 struct var_list {
   int ivar1, ivar2;
   float fvar1, fvar2;
+  std::vector<int> myVec;
   char *name;
 };
 
 
 void func( var_list *spnt){
   printf("inside the function %d, %d, %f, %f \n", spnt->ivar1, spnt->ivar2, spnt->fvar1, spnt->fvar2);
+
+  printf(" vector inside: ");
+  for (std::vector<int>::iterator it = spnt->myVec.begin(); it !=spnt->myVec.end(); ++it){
+    printf(" %d ", *it);
+  }
+  printf("\n");
+
   spnt->fvar1 = spnt->fvar1*2.0;
   spnt->fvar2 = spnt->fvar2*2.0;
 }
@@ -33,21 +44,30 @@ int main() {
 
   std::cout << " the code starts ...\n";
 
-  /*
-  check *ck;
-  ck->ivar1 = ipnt;
-  ck->ivar2 = ipnt;
-  printf("check %d, %d \n", ck->ivar1, ck->ivar2);
-  */
+  var_list v, *vl=&v;  // defining the pointer to the struct var_list
 
-  var_list v, *vl=&v;
+  // filling out the struct
   vl->ivar1 = 10;
   vl->ivar2 = 12;
 
   vl->fvar1 = 110.1;
   vl->fvar2 = 112.2;
+  
+  puts(" vector ");
+  vl->myVec.resize(10);
+  int i = 0;
+  for (std::vector<int>::iterator it = vl->myVec.begin(); it !=vl->myVec.end(); ++it){
+    *it = i++;
+  }
 
   printf("before %d, %d, %f, %f \n", vl->ivar1,vl->ivar2,vl->fvar1,vl->fvar2);
+  printf(" vector: ");
+  for (std::vector<int>::iterator it = vl->myVec.begin(); it !=vl->myVec.end(); ++it){
+    printf(" %d ", *it);
+  }
+  printf("\n");
+
+
   func(vl);
   printf("after 1 %d, %d, %f, %f \n", vl->ivar1,vl->ivar2,vl->fvar1,vl->fvar2);
   func2(vl);
