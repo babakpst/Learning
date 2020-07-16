3218d687f294763a16b9fff776381054ef7ae3e5
babakpst <babakpst@gmail.com>
2020-07-15 23:28:37 -0500

    GDB with struct and pointers

--- GDB/01_example1/src/main.cpp
+++ GDB/01_example1/src/main.cpp
@@ -14,11 +14,9 @@ check the "gdb (GNU DEBUGGER)"
 struct check {
   int ivar1, ivar2;
   float fvar1, fvar2;
-  char *name;
 };
 
 int mult(const int x, const int length) {
-
   int results = x * length;
   return results;
 }
@@ -29,27 +27,7 @@ int main() {
 
   int x = 3;
   int y, z;
-  int *ipnt1 = &y;
-
-  ipnt1 = &x;
-  int *ipnt2 = &y;
-  puts("before");
-  *ipnt2 = 4;
-  puts("after");
-  printf("pointer1 %d \n", *ipnt1);
-  printf("pointer2 %d \n", *ipnt2);
-
-  /*
-  check *ck;
-  ck->ivar1 = ipnt;
-  ck->ivar2 = ipnt;
-  printf("check %d, %d \n", ck->ivar1, ck->ivar2);
-  */
-  check ck2;
-  ck2.ivar1 = 10;
-  ck2.ivar2 = 12;
-  printf("check2 %d, %d \n", ck2.ivar1, ck2.ivar2);
-
+  puts("\n start... ");
   std::cout << " The value of x is: " << x << "\n";
 
   int length = -12;
@@ -64,6 +42,31 @@ int main() {
 
   std::cout << " z is: " << z << "\n";
   std::cout << " The division is: " << length / x << "\n";
+
+  // =============================================================================
+  puts(" pointer ");
+  int *ipnt1 = &y;
+  ipnt1 = &x;
+
+  int *ipnt2 = &y;
+  *ipnt2 = 4;
+
+  printf("pointer1 %d \n", *ipnt1);
+  printf("pointer2 %d \n", *ipnt2);
+
+  // =============================================================================
+  puts("\n  struct ");
+  check ck2;
+  ck2.ivar1 = 10;
+  ck2.ivar2 = 12;
+  printf("check2 %d, %d \n", ck2.ivar1, ck2.ivar2);
+
+  // =============================================================================
+  puts("\n pointer to struct ");
+  check ck, *pck = &ck;
+  pck->ivar1 = 456;
+  pck->ivar2 = 789;
+  printf("check %d, %d \n", pck->ivar1, pck->ivar2);
 
   std::cout << " Terminates successfully. "
             << "\n";
