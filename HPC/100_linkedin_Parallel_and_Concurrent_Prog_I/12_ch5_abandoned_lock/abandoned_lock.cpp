75afc2c2fa672b455c6e4b2d3cb533e1308729e4
babakpst <babakpst@gmail.com>
2020-07-22 11:33:56 -0500

    concurrent programming with c++ part 1

--- HPC/100_linkedin_Parallel_and_Concurrent_Prog_I/12_ch5_abandoned_lock/abandoned_lock.cpp
+++ HPC/100_linkedin_Parallel_and_Concurrent_Prog_I/12_ch5_abandoned_lock/abandoned_lock.cpp
@@ -1,6 +1,8 @@
 /**
  * Two philosophers, thinking and eating sushi
  */
+
+
 #include <thread>
 #include <mutex>
 
@@ -8,22 +10,33 @@ int sushi_count = 5000;
 
 void philosopher(std::mutex &chopsticks) {
     while (sushi_count > 0) {
+
+        //chopsticks.lock();
+        // by replacing the mutex to scoped_lock there is no deadlock
         std::scoped_lock lock(chopsticks);
+
         if (sushi_count) {
             sushi_count--;
         }
-        if (sushi_count == 10) {
-            printf("This philosopher has had enough!\n");
-            break;
-        }
+        // If we use the mutex lock and the following if statement, the first thread reaches the 10 sushi and who acquires the lock terminates before unlocking the lock. The other thread is wainting for a lock that is not going to release. 
+        if (sushi_count==10) {
+          puts(" this philosopher had enough");
+          break; 
+        } 
+        //chopsticks.unlock();
     }
 }
 
 int main() {
+
     std::mutex chopsticks;
+    
+
     std::thread barron(philosopher, std::ref(chopsticks));
     std::thread olivia(philosopher, std::ref(chopsticks));
+
     barron.join();
     olivia.join();
+
     printf("The philosophers are done eating.\n");
 }
--- HPC/100_linkedin_Parallel_and_Concurrent_Prog_I/13_ch5_starvation/starvation.cpp
+++ HPC/100_linkedin_Parallel_and_Concurrent_Prog_I/13_ch5_starvation/starvation.cpp
@@ -1,8 +1,12 @@
 /**
  * Too many philosophers, thinking and eating sushi
  */
+
 #include <thread>
 #include <mutex>
+#include <iostream>
+
+// If we have a large number of threads, some threads may never get the chance to acquire the lock. 
 
 int sushi_count = 5000;
 
@@ -15,17 +19,23 @@ void philosopher(std::mutex &chopsticks) {
             sushi_eaten++;
         }
     }
-    printf("Philosopher %d ate %d.\n", std::this_thread::get_id(), sushi_eaten);
+    //printf(" Philsopher %d eats %d \n", std::this_thread::get_id(), sushi_eaten);
+    std::cout << " Philsopher " << std::this_thread::get_id() << " eats " << sushi_eaten << "\n";
 }
 
+
 int main() {
+
     std::mutex chopsticks;
     std::array<std::thread, 200> philosophers;
+
     for (size_t i=0; i<philosophers.size(); i++) {
         philosophers[i] = std::thread(philosopher,std::ref(chopsticks));
     }
+
     for (size_t i=0; i<philosophers.size(); i++) {
         philosophers[i].join();
     }
+
     printf("The philosophers are done eating.\n");
 }
