ea6fa6f159640d554f0f04c8ec0385b37403fb75
babakpst <babakpst@gmail.com>
2020-11-29 22:35:41 -0600

    Leetcode: binary tree construction

--- C++/02_leetCode_GfG_Hacker/1/main.cpp
+++ C++/02_leetCode_GfG_Hacker/1/main.cpp
@@ -1,122 +1,91 @@
 
-/*
-Babak Poursartip
-11/02/2020
-
-Given an array of integers where 1 ≤ a[i] ≤ n (n = size of array), some elements
-appear twice and others appear once.
-
-Find all the elements of [1, n] inclusive that do not appear in this array.
-
-Could you do it without extra space and in O(n) runtime? You may assume the
-returned list does not count as extra space.
-
-Example:
-
-Input:
-[4,3,2,7,8,2,3,1]
-
-Output:
-[5,6]
-
-
-
-*/
-
-#include <algorithm>
-#include <iostream>
+#include <cmath>
+#include <cstdio>
 #include <vector>
-
+#include <iostream>
+#include <algorithm>
+#include <fstream>
 using namespace std;
 
-/*
-class Solution {
-public:
-    vector<int> findDisappearedNumbers(vector<int>& nums) {
-      std::vector<int> res;
-      for (int i=1; i<=nums.size(); ++i) {
-        std::vector<int>::iterator it=std::find(nums.begin(), nums.end(), i);
-        if (it==nums.end()) res.push_back(i);
-      }
-      return res;
-    }
-};
-*/
-//======================
-/*
-class Solution {
-public:
-  vector<int> findDisappearedNumbers(vector<int> &nums) {
-    std::vector<int> res;
-    std::sort(nums.begin(), nums.end());
-
-    std::cout << " the vector: \n";
-    for (auto c : nums)
-      std::cout << c << " ";
-    std::cout << std::endl;
-
-    int val = 1;
-    for (auto it = nums.begin(); it != nums.end(); ++it) {
-      if ((it + 1) < nums.end() && *it == *(it + 1))
-        continue;
-      while (*it != val) {
-        res.push_back(val);
-        val++;
-      }
-      val++;
-    }
-    for (int i = val; val <= nums.size(); val++)
-      res.push_back(val);
-
-    return res;
-  }
-};
-*/
-
-//======================
-class Solution {
-public:
-  vector<int> findDisappearedNumbers(vector<int> &nums) {
-    std::vector<int> res;
-
-    for (int i = 0; i < nums.size(); i++)
-      if (nums[abs(nums[i]) - 1] >= 0)
-        nums[abs(nums[i]) - 1] = -nums[abs(nums[i]) - 1];
-
-    /*
-    std::cout << " the vector: \n";
-    for (auto c : nums)
-      std::cout << c << " ";
-    std::cout << std::endl;
-    */
-    for (int i = 0; i < nums.size(); i++)
-      if (nums[i] > 0)
-        res.push_back(i + 1);
-
-    return res;
-  }
-};
-
-//============================
+///*
 int main() {
-  std::cout << " starts  here ... \n";
+    int m,n;
+    int p1, p2;
+    int minp, count, maxp;
 
-  // std::vector<int> myVec = {4, 3, 2, 7, 8, 2, 3, 1};
-  std::vector<int> myVec = {1, 1};
-  std::cout << " the vector: \n";
-  for (auto c : myVec)
-    std::cout << c << " ";
-  std::cout << std::endl;
+    ifstream input("file.txt");
+    ofstream output("out.txt");
+    input >> n >>m;
+    cout << " n: " << n <<  " m: " << m << endl;
+    vector<int> pp(n), cpp(n);
+    for (int i = 0; i<n; ++i) pp[i] = i+1;
+    for (int i = 0; i<m; ++i){
+        input >> p1 >> p2;
+        minp = (pp[p1-1]<pp[p2-1])? pp[p1-1]:pp[p2-1];
+        maxp = (pp[p1-1]>pp[p2-1])? pp[p1-1]:pp[p2-1];
+        cout << " p1: " << p1 <<  " p2: " << p2 << " min:" << minp << endl;
+        for (int ii = 0; ii<n; ++ii) {
+          if (pp[ii] == maxp) pp[ii] =minp;
+        }
+        pp[p1-1]=minp;
+        pp[p2-1]=minp;
 
-  // find the missing integers
-  Solution obj;
-  std::vector<int> res = obj.findDisappearedNumbers(myVec);
+        for (auto c :pp) cout << c << " ";
+        cout << endl;
 
-  std::cout << " missing integers: \n";
-  for (auto c : res)
-    std::cout << c << " ";
-  std::cout << std::endl;
+          if (p1>p2) {
+            count = 0;
+            cpp =pp;
+            sort(cpp.begin(),cpp.end());
+            for (int ii = 0; ii < n; ii++){
+              while (ii < n - 1 && cpp[ii] == cpp[ii + 1]){
+                ii++;
+                cout << ii << " "<< count << " here\n";
+              }
+            count++;
+            }
+          cout << " answer: " <<count << endl;
+          output <<count << endl;
+        }
+            
 
-  std::cout << "\n finished! \n";
-  return 0;
+    }
+    return 0;
 }
+//*/
+
+/*
+int main() {
+    int m,n;
+    int p1, p2;
+    int minp, count, maxp;
+    cin >> n >>m;
+    vector<int> pp(n), cpp(n);
+    for (int i = 0; i<n; ++i) pp[i] = i+1;
+    for (int i = 0; i<m; ++i){
+        cin >> p1 >> p2;
+        minp = (pp[p1-1]<pp[p2-1])? pp[p1-1]:pp[p2-1];
+        maxp = (pp[p1-1]>pp[p2-1])? pp[p1-1]:pp[p2-1];
+
+        for (int ii = 0; ii<n; ++ii) {
+          if (pp[ii] == maxp) pp[ii] =minp;
+        }
+        pp[p1-1]=minp;
+        pp[p2-1]=minp;
+
+          if (p1>p2) {
+            count = 0;
+            cpp =pp;
+            sort(cpp.begin(),cpp.end());
+            for (int ii = 0; ii < n; ii++){
+              while (ii < n - 1 && cpp[ii] == cpp[ii + 1]){
+                ii++;
+              }
+            count++;
+            }
+          cout <<count << endl;
+        }
+    }
+    return 0;
+}
+*/
