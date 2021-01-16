229503ae1ef65530910c78bc627d791ff07a77a4
babakpst <babakpst@gmail.com>
2021-01-16 12:10:12 -0600

    unique_ptr

--- C++/00_general/34_UniquePointer/main.cpp
+++ C++/00_general/34_UniquePointer/main.cpp
@@ -7,6 +7,8 @@
 #include <memory>
 #include <string>
 
+
+// ========================================
 class model {
 private:
   std::unique_ptr<double> pntr_cls;
@@ -17,13 +19,28 @@ public:
   void print_cls() { std::cout << " print: " << *pntr_cls << "\n"; };
 };
 
+
+// ========================================
+void f(std::unique_ptr<int> &a){
+
+std::cout << " the current value of the unique_ptr inside the function is: " << *a << std::endl;
+
+*a = 10;
+
+std::cout << " the current value of the unique_ptr inside the function after change is: " << *a << std::endl;
+
+}
+
+// ==============================
 int main() {
 
+
+  // 1 =====================
+  std::cout << "1 =========\n";
   std::unique_ptr<double> upnt{new double};
   *upnt = 12;
-
   std::cout << " The unique pointer is defined as: " << *upnt << "\n";
-  // std::cout << " The unique pointer is defined as: " <<  "\n";
+
 
   // double dvar =5.0;
   // std::unique_ptr<double> upnt2{&dvar};   // wrong allocation
@@ -34,17 +51,39 @@ int main() {
 
   // std::unique_ptr<double> upnt2{upnt};   wrong
 
+  std::cout << "2 =========\n";
   std::unique_ptr<double> upnt3;
   // upnt3 = upnt; wrong, you cannot assign it
 
-  double *raw_pntr = upnt.get();
-  std::cout << " This is a raw pointer: " << *raw_pntr << "\n";
 
+  double *raw_pntr(new double);
+  raw_pntr = upnt.get();
+  std::cout << " This is a raw pointer: " << *raw_pntr << "\n";
+  *raw_pntr=15;
+  std::cout << " This is a raw pointer: " << *raw_pntr << "\n";
+  std::cout << " The unique pointer is defined as: " << *upnt << "\n";
+  raw_pntr = nullptr; // we need this otherwise it would a double free.
+  delete raw_pntr;
+
+  std::cout << "3 =========\n";
   // class
   double a_num = 12.2;
 
+  /*
   model model_obj(a_num);
   model_obj.print_cls();
+  */
 
+
+  
+  std::cout << "4 =========\n";
+  // pass to a function ======== 
+  std::unique_ptr<int> mUP(new int(5));
+  std::cout << " the value of the unique_ptr in the main function is: " << *mUP << std::endl;
+  f(mUP);
+  std::cout << " the value of the unique_ptr in the main function after passing to the func is: " << *mUP << std::endl;  
+
+
+  std::cout << "\n finished successfully\n";
   return 0;
 }
