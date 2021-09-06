fe6ebbb1b68258ef6a21459ac1cc978c9ea898d1
babakpst <babakpst@gmail.com>
2021-09-06 10:07:15 -0500

    static assert vs assert

--- C++/00_general/01_assert/main.cpp
+++ C++/00_general/01_assert/main.cpp
@@ -23,9 +23,23 @@ int main(){
 
   int length = -12;
   std::cout << " Here is the length: " << length << "\n";
+  
+  //assert(4<3); // execution stops here, if uncomment
+  std::cout << " here is passed\n";
+
+  //assert(x<length);// execution stops here, if uncomment
+  std::cout << " here is passed\n";
+
+
+  // static_assert
+  //static_assert(2>3, "four is greater than 3"); // execution stops here, if uncomment
+  std::cout << " static_assert\n";
+  
+  
   assert("Length should be positive!" && length>=0);
   std::cout << " passed the assert. \n";
 
-  
+
+    
   return 0;
 }
