113cced894e059e26daf082b18edde37233a3f19
babakpst <babakpst@gmail.com>
2022-09-08 08:42:08 -0500

    leetcode practice traverse a tree

---
+++ C++/02_leetCode_GfG_Hacker/l057_count_univalue_subtrees/main.cpp
@@ -0,0 +1,28 @@
+
+// babak poursartip
+// Aug 28, 2022
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+
+
+int main(int argc, char* argv[])
+{
+  std::cout << " starts ... \n";
+
+ 
+ 
+  Solution t;
+
+  string out = t.
+  cout << " answer: " << out << endl;
+  cout << " answer: "<< endl;
+  for_each(out.begin(), out.end(), [](auto x){cout << x << " ";});
+  cout << endl;
+  std::cout << " done \n";
+  return 0;
+}
