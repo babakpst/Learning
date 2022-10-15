618f08590972d2d5d774f208d0c1276cade7341f
babakpst <babakpst@gmail.com>
2022-10-15 17:32:35 -0500

    leetcode binary works

--- C++/02_leetCode_GfG_Hacker/l087_search_in_a_sorted_array/main.cpp
+++ C++/02_leetCode_GfG_Hacker/l087_search_in_a_sorted_array/main.cpp
@@ -22,23 +22,7 @@ using namespace std;
 class Solution {
 public:
     int search(const ArrayReader& reader, int target) {
-      
-      int left = 0, right =10000;
-      while(left <= right) 
-      {
-        int mid = left + (right-left)/2;
-        int num = reader.get(mid);
-        if ( num == target) return mid;
-        else if ( num < target ) left = mid + 1;
-        else if ( num > target ) right = mid -1;
-      }
-      return -1;
-    }
-};
-
-
-int main(int argc, char* argv[])
-{
+f the target value is equal to the middle element - we're done.
   std::cout << " starts ... \n";
 
   const int reader
---
+++ C++/02_leetCode_GfG_Hacker/l089_find_smallest_letter/main.cpp
@@ -0,0 +1,59 @@
+
+// babak poursartip
+// Aug 28, 2022
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+
+class Solution {
+public:
+    char nextGreatestLetter(vector<char>& letters, char target) {
+      int size = letters.size();
+
+      if (letters[size-1]-'a'<= target-'a') return letters[0];
+      
+      int left = 0, right = size -1;
+      while (left<right)
+      {
+        int mid = left + (right-left)/2;
+        
+        cout << left << " " << mid << " " << right << endl;
+        
+        if (letters[mid]-'a'<= target-'a'  && letters[mid+1]-'a' > target-'a' ) return letters[mid + 1];
+        else if (letters[mid]-'a'<= target-'a') left = mid;
+        else if (letters[mid]-'a'>= target-'a') right = mid;
+      }
+      return letters[0];
+    }
+};
+
+
+
+int main(int argc, char* argv[])
+{
+  std::cout << " starts ... \n";
+
+  // vector<char> letters={'c','f','j'};
+  // int target = 'f';
+ 
+  vector<char> letters={'e','e','e','e','e','e','n','n','n','n'};
+  int target = 'e';
+
+
+
+
+
+  Solution t;
+
+  char out = t.nextGreatestLetter(letters, target);
+  
+  cout << " answer: " << out << endl;
+  
+  
+  std::cout << " done \n";
+  return 0;
+}
