83c9b203db19c6d4f7a02df10dc36ae806ee1b0d
babakpst <babakpst@gmail.com>
2022-11-16 08:24:32 -0600

    leetcode sorting

--- C++/02_leetCode_GfG_Hacker/l095_top_k_frequent_element/main.cpp
+++ C++/02_leetCode_GfG_Hacker/l095_top_k_frequent_element/main.cpp
@@ -3,12 +3,14 @@
 // Aug 28, 2022
 
 #include <iostream>
+#include <utility>
 #include <vector>
 #include <algorithm>
 #include <map>
 #include <climits>
 
 using namespace std;
+
 /*
 class Solution {
 public:
@@ -43,6 +45,11 @@ public:
 };
 */
 
+struct compare
+{
+  bool operator()(pair<int,int> a,pair<int,int> b) const {return a.second<b.second; }
+};
+
 class Solution {
 public:
     vector<int> topKFrequent(vector<int>& nums, int k) 
@@ -51,26 +58,42 @@ public:
       map<int,int> freq;
       
       for (vector<int>::iterator it = nums.begin(); it != nums.end(); it++)
+        freq[*it] +=1;
+
+
+      vector<pair<int,int>> myHeap(freq.size());
       {
-        if (freq.find(*it) == freq.end() ) 
-          freq[*it] = 1;
-        else
-          freq[*it] =freq[*it]+1;
+      int i = 0;
+      for ( auto it = freq.begin(); it != freq.end(); it++, i++ )
+        myHeap[i] = make_pair(it->first, it->second);
       }
 
-      vector<int> out(k,INT_MIN);
+      // cout << " output before heapify \n";
+      // for_each( myHeap.begin(), myHeap.end(), [](auto x){std::cout << x.first << " " << x.second << std::endl;} );      
+      // cout << " =========== \n";
+
+      make_heap(myHeap.begin(), myHeap.end(), compare());
+      // cout << " output after heapify \n";
+      // for_each( myHeap.begin(), myHeap.end(), [](auto x){std::cout << x.first << " " << x.second << std::endl;} );      
+      // cout << " =========== \n";
+
+
+      auto end = myHeap.end();
       for (int i = 0; i<k; i++)
       {
-        int max = freq.begin()->first;
-        for ( auto it = freq.begin(); it != freq.end(); it++ )
-        {
-          // cout << k << " -  " <<  it->first << " " << it->second << endl;
-          if ( it->second > freq[max] ) 
-            max = it->first;        
-        }
-        out[i] = max;
-        freq.erase(max);
-      }     
+        std::pop_heap(myHeap.begin(), end, compare());
+        end--;
+
+        // cout << " output after heapify \n";
+        // for_each( myHeap.begin(), myHeap.end(), [](auto x){std::cout << x.first << " " << x.second << std::endl;} );      
+        // cout << " =========== \n";
+
+      }
+      
+      vector<int> out(k,INT_MIN);
+      for (int i = 0; i<k; i++, end++)
+        out[i] = end->first;
+
       return out;
     }
 };
@@ -87,8 +110,14 @@ int main(int argc, char* argv[])
 
   // vector<int> nums{0,1,1,1,2,2,3};
   // vector<int> nums{-1,0,1,1,1,1,2,3,3};
-  vector<int> nums{-1,0,2,3,3,1,1,1,1};
+  // vector<int> nums{-1,0,2,3,3,1,1,1,1};
+  // int k = 2;
+
+  vector<int> nums{4,1,-1,2,-1,2,3};
   int k = 2;
+
+
+
 
 
   Solution t;
