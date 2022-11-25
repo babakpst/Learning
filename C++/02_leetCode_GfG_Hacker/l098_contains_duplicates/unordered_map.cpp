e0ca99dd1b45a2026f326d77f7c8adf634f53c22
babakpst <babakpst@gmail.com>
2022-11-25 12:49:07 -0600

    Leetcode: hash practice

---
+++ C++/02_leetCode_GfG_Hacker/l098_contains_duplicates/unordered_map.cpp
@@ -0,0 +1,34 @@
+
+#include <iostream>
+#include <unordered_set>                // 0. include the library
+
+using namespace std;
+
+int main() {
+    // 1. initialize a hash set
+    unordered_set<int> hashset;   
+    // 2. insert a new key
+    hashset.insert(3);
+    hashset.insert(3);
+    hashset.insert(2);
+    hashset.insert(1);
+    // 3. delete a key
+    hashset.erase(2);
+    // 4. check if the key is in the hash set
+    if (hashset.count(2) <= 0) {
+        cout << "Key 2 is not in the hash set." << endl;
+    }
+    // 5. get the size of the hash set
+    cout << "The size of hash set is: " << hashset.size() << endl; 
+    // 6. iterate the hash set
+    for (auto it = hashset.begin(); it != hashset.end(); ++it) {
+        cout << (*it) << " ";
+    }
+    cout << "are in the hash set." << endl;
+    // 7. clear the hash set
+    hashset.clear();
+    // 8. check if the hash set is empty
+    if (hashset.empty()) {
+        cout << "hash set is empty now!" << endl;
+    }
+}
---
+++ C++/02_leetCode_GfG_Hacker/l099_single_number/main.cpp
@@ -0,0 +1,41 @@
+
+// babak poursartip
+// Nov 23, 2022
+
+#include <iostream>
+#include <iterator>
+#include <vector>
+#include <algorithm>
+#include <unordered_set>
+
+using namespace std;
+
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+      unordered_set<int> mySet;
+      for (auto it = begin(nums); it!=end(nums); it++)
+        if (mySet.find(*it) !=end(mySet))
+          mySet.erase(*it);
+        else
+          mySet.insert(*it);
+        return *begin(mySet);
+    }
+};
+
+int main(int argc, char* argv[])
+{
+  std::cout << " starts ... \n";
+
+  // vector<int> nums{2,2,1}; 
+  vector<int> nums{4,1,2,1,2}; 
+ 
+  Solution t;
+
+  int out = t.singleNumber(nums);
+  
+  cout << " answer: " << out << endl;
+  
+  std::cout << " done \n";
+  return 0;
+}
