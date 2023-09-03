16c589fa5e33a51a3ed780a640dd93609b43451a
babakpst <babakpst@gmail.com>
2023-09-03 10:12:02 -0500

    house robber

--- C++/02_leetCode_GfG_Hacker/l017_house_robber/main.cpp
+++ C++/02_leetCode_GfG_Hacker/l017_house_robber/main.cpp
@@ -9,32 +9,47 @@
 
 using namespace std;
 
-class Solution {
-public:
-    int rob(vector<int>& nums) {
-
-        std::vector<int> money(nums.size(),0);
-        for (int i = 0; i<nums.size(); i++)
-        {
-          if (i == 0)
-            money[i] = nums[i];
-          else if (i == 1)
-            money[i] = max(nums[i], nums[i-1]);
-          else
-            money[i] = max(money[i-1], money[i-2]+nums[i]);
-        }
-
-        return money[nums.size()-1];
+class Solution1
+{
+ public:
+  int rob(vector<int>& nums)
+  {
+    std::vector<int> money(nums.size(), 0);
+    for (int i = 0; i < nums.size(); i++)
+    {
+      if (i == 0)
+        money[i] = nums[i];
+      else if (i == 1)
+        money[i] = max(nums[i], nums[i - 1]);
+      else
+        money[i] = max(money[i - 1], money[i - 2] + nums[i]);
     }
+
+    return money[nums.size() - 1];
+  }
 };
 
+class Solution
+{
+ public:
+  int rob(vector<int>& nums)
+  {
+    std::vector<int> money(nums.size(), 0);
+
+    money[0] = nums[0];
+    money[1] = max(nums[0], nums[1]);
+
+    for (int i = 0; i < nums.size(); i++) money[i] = max(money[i - 1], money[i - 2] + nums[i]);
+    return money[nums.size() - 1];
+  }
+};
 
 int main(int argc, char* argv[])
 {
   std::cout << " starts ... \n";
 
-  //std::vector<int> vec{1,2,3,1};
-  std::vector<int> vec{2,7,9,3,1};  
+  // std::vector<int> vec{1,2,3,1};
+  std::vector<int> vec{2, 7, 9, 3, 1};
 
   Solution t;
   std::cout << " output: " << t.rob(vec) << std::endl;
