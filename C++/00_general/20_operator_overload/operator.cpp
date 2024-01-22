ed9a6ff176d5e3fe68801024e6db2364925516fb
babakpst <babakpst@gmail.com>
2024-01-22 08:29:59 -0600

    c++ operator overload

--- C++/00_general/20_operator_overload/operator.cpp
+++ C++/00_general/20_operator_overload/operator.cpp
@@ -12,87 +12,112 @@ geeksforgeeks
 #include <iostream>
 
 // ============================================================
-class complex {
-private:
+class complex
+{
+ private:
   int real, imag;
 
-public:
-  complex();
-  complex(int, int);
+ public:
+  complex();          // ctor
+  complex(int, int);  // ctor
+  // complex(const complex&); // copy ctor
+  // const complex &operator=(const complex &);  // copy assignment
+
   complex operator+(const complex &) const;
+  // complex operator+(const complex &);
+  complex &operator+=(const complex &);
   void print() const;
 };
 
 complex::complex(int real = 0, int imag = 0) : real(real), imag(imag){};
 
+// const complex &operator=(const complex &);  // copy assignment
+
+complex &complex::operator+=(const complex &rhs)
+{
+  std::cout << rhs.real << " +i" << rhs.imag << std::endl;
+  std::cout << this->real << " +i" << this->imag << std::endl;
+  this->real += rhs.real;
+  this->imag += rhs.imag;
+  return *this;
+  // return complex(real+=rhs.real, imag+=rhs.imag);
+}
+
+complex complex::operator+(complex const &rhs) const { return complex(real + rhs.real, imag + rhs.imag); };
+// complex complex::operator+(const complex &rhs){return (*this)+=rhs;} // Wrong, it changes both right and left hand
+// side.
+
 void complex::print() const { std::cout << real << " + " << imag << "i\n"; };
-
-// This func will be called when '+' appears btw the two complex numbers.
-complex complex::operator+(complex const &obj) const {
-  return complex(real + obj.real, imag + obj.imag);
-};
-
 
 // ============================================================
 class vec
 {
-private: 
-//
-public:
+ private:
+  //
+ public:
   float _x, _y;
 
-vec(){};
-vec(const float x, const float y): _x{x}, _y{y}{};
+  vec(){};
+  vec(const float x, const float y) : _x{x}, _y{y} {};
 
-vec operator+(const vec & other){
-return vec(_x+other._x,_y+other._y);
-};
+  vec operator+(const vec &other) { return vec(_x + other._x, _y + other._y); };
 
-bool operator==(const vec &rhs){
-  return (_x==rhs._x && _y==rhs._y);
-}
+  bool operator==(const vec &rhs) { return (_x == rhs._x && _y == rhs._y); }
 
-bool operator!=(const vec &rhs){
-  return !(*this==rhs);
-}
+  bool operator!=(const vec &rhs) { return !(*this == rhs); }
 
-void print(std::string title,const vec &other){std::cout<< title<<"=(" <<_x << "," <<_y << ")" << std::endl;};
-
+  void print(std::string title, const vec &other)
+  {
+    std::cout << title << "=(" << _x << "," << _y << ")" << std::endl;
+  };
 };
 
 // ============================================================
-std::ostream& operator<<(std::ostream& stream, const vec &other){
+std::ostream &operator<<(std::ostream &stream, const vec &other)
+{
   stream << "(" << other._x << "," << other._y << ")";
   return stream;
 }
 
-
 // ============================================================
-int main() {
-
+int main()
+{
   // ================================
-  complex a(4, 5), b(2, 3), c(0, 0);
+  complex a(4, 5), b(2, 3), c(1, 1);
   a.print();
   b.print();
 
+  std::cout << "here is c+=\n";
+  c += a;
+  c.print();
+  a.print();
+
+  std::cout << "here is c\n";
   c = a + b;
+  c.print();
+  a.print();
+  b.print();
+
+  std::cout << "here is c+=\n";
+  c += b;
   c.print();
 
   // ================================
-  vec avec(1.0,1.1), bvec(2.1,2.2), cvec;
+  std::cout << "\n test vector summation \n ";
+  vec avec(1.0, 1.1), bvec(2.1, 2.2), cvec;
   cvec = avec + bvec;
   avec.print("a", avec);
   bvec.print("b", bvec);
   cvec.print("c", cvec);
-   
+
   std::cout << " this is the operator overload: " << cvec << std::endl;
-  bool eq1= cvec == avec;
+  bool eq1 = cvec == avec;
   std::cout << " == operator overload: " << eq1 << std::endl;
 
-  bool eq2= cvec != avec;
+  bool eq2 = cvec != avec;
   std::cout << " == operator overload: " << eq2 << std::endl;
 
-  std::cout << " == operator overload: " << (avec == avec) << std::endl;    
+  std::cout << " == operator overload: " << (avec == avec) << std::endl;
 
   return 0;
 }
