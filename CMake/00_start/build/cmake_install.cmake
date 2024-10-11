27d32f0f343a0f98daadfa67dd8696c004716492
babakpst <babakpst@gmail.com>
2024-10-11 08:28:38 -0500

    update the cmake test files

--- CMake/00_start/build/cmake_install.cmake
+++ CMake/00_start/build/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /home/babak/Codes/Learning/CMake/00_start
+# Install script for directory: /home/babak/workspace/Learning/CMake/00_start
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -45,5 +45,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "/home/babak/Codes/Learning/CMake/00_start/build/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "/home/babak/workspace/Learning/CMake/00_start/build/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
