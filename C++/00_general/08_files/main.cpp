b29c4b7a9fd0efb1d2493eef1b49036829b44cd2
babakpst <babakpst@gmail.com>
2025-04-16 08:32:53 -0500

    io C++

--- C++/00_general/08_files/main.cpp
+++ C++/00_general/08_files/main.cpp
@@ -4,9 +4,8 @@
 #include <iostream>
 #include <string>
 
-
-int main() {
-
+int main()
+{
   //====================================================================
   // prone to error if the file does not exist
   // method 1: we do not need to close the file, it automatically closes when it goes out of scope
@@ -14,16 +13,26 @@ int main() {
 
   // method 2:
   std::fstream read_file;
-  read_file.open("read_from_file.xyz");
+  read_file.open("read_from_file.xyz", std::ios::in);
 
   int index;
   double res;
 
-  for (int i = 0; i < 4; i++) {
+  for (int i = 0; i < 4; i++)
+  {
     read_file >> index >> res;
     std::cout << " the index: " << index << " result is: " << res << "\n";
   }
 
+  // read_file.rewind(); //not working
+  // read_file.open("read_from_file.xyz", std::ios::in);
+  for (int i = 0; i < 4; i++)
+  {
+    read_file >> index >> res;
+    std::cout << " the index: " << index << " result is: " << res << "\n";
+  }
+
+  read_file.close();
   std::cout << "The end of the code! \n";
   std::cout << "done! \n";
 
@@ -31,11 +40,18 @@ int main() {
   // will throw error
   std::ifstream Addressfile;
   Addressfile.exceptions(std::ifstream::failbit | std::ifstream::badbit);
-  try {
+  std::string str;
+  try
+  {
     Addressfile.open("Address.txt", std::ios::in);
-  } catch (std::ifstream::failure &inputerr) { 
-  // here is the catch for opening the input file.
-    std::cout << " Error while opening the address file. " << "\n";
+    Addressfile >> str;
+    std::cout << " content of the Address file: " << str << std::endl;
+  }
+  catch (std::ifstream::failure &inputerr)
+  {
+    // here is the catch for opening the input file.
+    std::cout << " Error while opening the address file. "
+              << "\n";
     std::cout << " Please double check the address file in the cpp folder. "
               << "\n";
     std::cout << " The error message is: " << inputerr.what() << "\n";
@@ -52,22 +68,19 @@ int main() {
   std::cout << filename << "\n";
   bool opened = false;
 
-  while (!opened) {
-
+  while (!opened)
+  {
     infile.open(filename.c_str());
 
-    if (infile.good()) {
+    if (infile.good())
+    {
       opened = true;
-    } else {
-      std::cout << " The file '" << filename
-                << " ' does not exist, give a new file name: ";
+    }
+    else
+    {
+      std::cout << " The file '" << filename << " ' does not exist, give a new file name: ";
       std::cin >> filename;
     }
-  }
-
-  for (int i = 0; i < 4; i++) {
-    read_file >> index >> res;
-    std::cout << " the index: " << index << " result is: " << res << "\n";
   }
 
   std::cout << "The end of the code! \n";
